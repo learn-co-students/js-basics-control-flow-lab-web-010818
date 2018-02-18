1ea99f212b3bb11cdf0c908924a4b8d2998044f8
TorreJohnson <torre.johnson@gmail.com>
2018-02-18 16:57:48 -0500

    completed Control Flow Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!'
+  } else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+};
+
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
