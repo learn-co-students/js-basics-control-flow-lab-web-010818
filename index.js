55653e81b460777d3ddc815da373dad079f4f6ac
github username <github email address>
2018-02-18 15:39:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num) {
+  let greeting;
+  if (num < 400){
+    greeting = "This one is on me!"
+  } else if (num > 2000 && num <= 2500){
+    greeting = "I will gladly take your thirty bucks."
+  } else if (num > 2500){
+    greeting = 'No can do.'
+  }
+  return greeting;
+}
+
+function ternaryCheckCity(city){
+  let response;
+   response =  (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+   return response;
+}
+
+function switchOnCharmFromTip(tip){
+  let response = '';
+
+  switch (tip){
+    case 'generous':
+    response = 'Thank you so much.';
+    break;
+    case 'not as generous':
+    response = 'Thank you.';
+    break;
+    default:
+    response = 'Bye.'
+  }
+  return response;
+}
